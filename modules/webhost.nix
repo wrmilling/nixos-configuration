b5f79afd257c84a9cb1fadbedcaa9f054dc824dc
Winston R. Milling <Winston@Milli.ng>
2022-11-03 11:15:27 -0500

    Hermes: Add ACME email

--- modules/webhost.nix
+++ modules/webhost.nix
@@ -5,6 +5,7 @@ let secrets = import ../secrets.nix; in
 {
   services.nginx.enable = true;
   security.acme.acceptTerms = true;
+  security.acme.defaults.email = "Winston@${secrets.DOMAIN}";
   services.nginx.virtualHosts."${secrets.DOMAIN}" = {
     forceSSL = true;
     enableACME = true;
