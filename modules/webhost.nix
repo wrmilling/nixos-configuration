70e662bdb23ed134718919554385e7999a862138
Winston R. Milling <Winston@Milli.ng>
2022-11-03 11:13:33 -0500

    Hermes: Accept ACME TOS

--- modules/webhost.nix
+++ modules/webhost.nix
@@ -3,7 +3,8 @@
 let secrets = import ../secrets.nix; in
 
 {
-  services.nginx.enable = true; 
+  services.nginx.enable = true;
+  security.acme.acceptTerms = true;
   services.nginx.virtualHosts."${secrets.DOMAIN}" = {
     forceSSL = true;
     enableACME = true;
