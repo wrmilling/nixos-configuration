561cb3bcd2f9fb865d30d04b77bbb1a85aa4185a
Winston R. Milling <Winston@Milli.ng>
2023-06-18 15:46:07 -0500

    HomeManager: Migrating some graphical apps

--- home-manager/features/graphical/default.nix
+++ home-manager/features/graphical/default.nix
@@ -5,6 +5,24 @@
     ./alacritty.nix
   ];
   home.packages = with pkgs; [
-    
+    # Web
+    (vivaldi.override {
+      proprietaryCodecs = true;
+      enableWidevine = false;
+    })
+
+    # Comms
+    element-desktop
+    discord
+    slack
+
+    # 3-D Printing
+    cura
+
+    # Utils
+    flameshot
+    gparted
+    volumeicon
+    pinentry-rofi
   ];
 }
--- machines/common/modules/graphical.nix
+++ machines/common/modules/graphical.nix
@@ -4,19 +4,5 @@
   environment.systemPackages = with pkgs; [
     mesa
     glxinfo
-    alacritty
-    (vivaldi.override {
-      proprietaryCodecs = true;
-      enableWidevine = false;
-    }) # Won't work on ARM64, but I am ignoring that for now.
-    flameshot
-    element-desktop
-    discord
-    slack
-    gparted
-    gramps
-    volumeicon
-    pinentry-rofi
-    cura
   ];
 }
