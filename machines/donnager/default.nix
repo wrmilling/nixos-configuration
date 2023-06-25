e7867f21e3a360cc4f02a90e442e0a5c1e524e97
Winston R. Milling <Winston@Milli.ng>
2023-06-25 10:55:04 -0500

    Boot: Add plymouth

--- machines/donnager/default.nix
+++ machines/donnager/default.nix
@@ -27,6 +27,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.plymouth.enable = true;
   # boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
 
   networking = {
