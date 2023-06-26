6a95d9e5d935c5891a44ff0b126b79a02faffbf4
Winston R. Milling <Winston@Milli.ng>
2023-06-26 18:45:09 -0500

    Darwin: Update defaults

--- home-manager/darwin/default.nix
+++ home-manager/darwin/default.nix
@@ -1,44 +1,20 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{ inputs, outputs, secrets, lib, config, pkgs, ... }:
 
-{ inputs, outputs, secrets, lib, config, pkgs, ... }: {
-  # You can import other home-manager modules here
+{
   imports = [
     # ../common/terminal
     # ../common/graphical
     # ../common/scripts
-
-    # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
-
-    # Or modules exported from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModules.default
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
   ];
 
   nixpkgs = {
-    # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       # outputs.overlays.modifications
       outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
-    # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = (_: true);
@@ -46,13 +22,9 @@
   };
 
   home = {
-    username = secrets.machines.work-mac.username;
-    homeDirectory = "/Users/${secrets.machines.work-mac.username}";
+    username = lib.mkDefault "WRM6768";
+    homeDirectory = lib.mkDefault "/Users/WRM6768";
   };
-
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
 
   # Enable home-manager
   programs.home-manager.enable = true;
--- machines/darwin/default.nix
+++ machines/darwin/default.nix
@@ -3,24 +3,15 @@
 {
   imports = 
   [
-    inputs.home-manager.nixosModules.home-manager
+    inputs.home-manager.darwinModules.home-manager
   ];
-
-  # Make sure the nix daemon always runs
-  services.nix-daemon.enable = true;
-  # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
-  services.nix-daemon.package = pkgs.nixFlakes;
   
-  # if you use zsh (the default on new macOS installations),
-  # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
-  programs.zsh.enable = true;
-  # bash is enabled by default
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nixVersions.nix_2_16;
+  security.pki-certificateFiles = [ "/etc/ssl/thd_combined.pem" ];
 
   home-manager = {
     extraSpecialArgs = { inherit inputs outputs secrets; };
-    users = {
-      "${secrets.machines.work-mac.username}" = import ../../home-manager/darwin;
-    };
+    users.WRM6768 = import ../../home-manager/darwin;
   };
-
 }
