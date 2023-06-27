d94015860d3a49656b349087e7108893bc830d0b
Winston R. Milling <Winston@Milli.ng>
2023-06-26 19:01:30 -0500

    Darwin: Separate home manager and nix for now

--- machines/darwin/default.nix
+++ machines/darwin/default.nix
@@ -5,13 +5,62 @@
   [
     inputs.home-manager.darwinModules.home-manager
   ];
-  
-  services.nix-daemon.enable = true;
-  nix.package = pkgs.nixVersions.nix_2_16;
-  security.pki-certificateFiles = [ "/etc/ssl/thd_combined.pem" ];
 
-  home-manager = {
-    extraSpecialArgs = { inherit inputs outputs secrets; };
-    users.WRM6768 = import ../../home-manager/darwin;
+  security.pki.certificateFiles = [ "/etc/ssl/thd_combined.pem" ];
+
+  # home-manager = {
+  #   extraSpecialArgs = { inherit inputs outputs secrets; };
+  #   users.WRM6768 = import ../../home-manager/darwin;
+  # };
+
+  #package config
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+    };
   };
+
+  nix = {
+    # package = lib.mkDefault pkgs.nix;
+    package = pkgs.nixVersions.nix_2_16;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      warn-dirty = false;
+      sandbox = "relaxed";
+    };
+
+    configureBuildUsers = true;
+
+    gc = {
+      automatic = true;
+      interval = { Weekday = 0; Hour = 2; Minute = 0; };
+      options = "--delete-older-than 30d";
+    };
+  };
+
+  services.activate-system.enable = true;
+  services.nix-daemon.enable = true;
+  programs.nix-index.enable = true;
+
+  environment = {
+    systemPackages = [
+        pkgs.coreutils
+        pkgs.fish
+        pkgs.git
+        pkgs.vim
+        pkgs.home-manager
+        ];
+    shells = [ pkgs.bashInteractive pkgs.fish ];
+    variables.EDITOR = "${lib.getBin pkgs.vim}/bin/vim";
+  };
+
+  programs = {
+    bash.enable = true;
+    fish.enable = true;
+    zsh.enable = true;
+  };
+
+  # Use touch ID for sudo auth
+  security.pam.enableSudoTouchIdAuth = true;
 }
