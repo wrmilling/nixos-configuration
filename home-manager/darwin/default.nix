124701091bddbbeb7b7c0df2c5d1696290500d59
Winston R. Milling <Winston@Milli.ng>
2023-06-27 11:21:49 -0500

    Darwin: Add fish links for nix-darwin

--- home-manager/darwin/default.nix
+++ home-manager/darwin/default.nix
@@ -28,6 +28,25 @@
   # Enable home-manager
   programs.home-manager.enable = true;
 
+  # Fish Additions
+  programs.fish = {
+    loginShellInit = ''for p in (string split " " $NIX_PROFILES); fish_add_path --prepend --move $p/bin; end'';
+    interactiveShellInit =
+      # fix brew path (should not be needed but somehow is?)
+      ''
+      eval (/opt/homebrew/bin/brew shellenv)
+      '' +
+      # handle gcloud CLI
+      ''
+      bass source ~/google-cloud-sdk/path.bash.inc
+      bass source ~/google-cloud-sdk/completion.bash.inc
+      '' +
+      # add custom localized paths
+      ''
+      fish_add_path $HOME/.rd/bin
+      '';
+  };
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
 }
