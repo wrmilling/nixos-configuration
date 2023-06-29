5971e2edc34adcc91d0d60516fa4c838401fc04f
Winston R. Milling <Winston@Milli.ng>
2023-06-29 14:23:50 -0500

    Flake: Update lock, clean format

--- flake.nix
+++ flake.nix
@@ -18,88 +18,74 @@
     darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, darwin, ... }@inputs:
-    let
-      inherit (self) outputs;
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
-      secrets = import ./secrets.nix;
-    in
-    rec {
-      # Custom Packages
-      packages = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in import ./custom/pkgs { inherit pkgs; }
-      );
+  outputs = { 
+    self, 
+    nixpkgs, 
+    home-manager, 
+    darwin, 
+    ... 
+  } @ inputs: let
+    inherit (self) outputs;
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    secrets = import ./secrets.nix;
+  in rec {
+    # Custom Packages
+    packages = forAllSystems (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in import ./custom/pkgs { inherit pkgs; }
+    );
 
-      # Devshell for bootstrapping
-      # Acessible through 'nix develop' or 'nix-shell' (legacy)
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in import ./shell.nix { inherit pkgs; }
-      );
+    # Devshell for bootstrapping
+    # Acessible through 'nix develop' or 'nix-shell' (legacy)
+    devShells = forAllSystems (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in import ./shell.nix { inherit pkgs; }
+    );
 
-      # Overlays
-      overlays = import ./custom/overlays { inherit inputs; };
+    # Overlays
+    overlays = import ./custom/overlays { inherit inputs; };
 
-      # Reusable nixos modules you might want to export
-      # These are usually stuff you would upstream into nixpkgs
-      # nixosModules = import ./modules/nixos;
-
-      # Reusable home-manager modules you might want to export
-      # These are usually stuff you would upstream into home-manager
-      # homeManagerModules = import ./modules/home-manager;
-
-      # NixOS configuration entrypoint
-      # Available through 'nixos-rebuild switch --flake .#your-hostname'
-      nixosConfigurations = {
-        donnager = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs packages secrets; };
-          modules = [
-            ./hosts/donnager
-          ];
-        };
-        hermes = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs packages secrets; };
-          modules = [
-            ./hosts/hermes
-          ];
-        };
-        serenity = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs packages secrets; };
-          modules = [
-            ./hosts/serenity
-          ];
-        };
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild switch --flake .#your-hostname'
+    nixosConfigurations = {
+      donnager = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs outputs packages secrets; };
+        modules = [ ./hosts/donnager ];
       };
-
-      # nix-darwin configuration entrypoint
-      # Available through 'darwin-rebuild switch --flake .#your-hostname'
-      darwinConfigurations = {
-        "${secrets.hosts.work-mac.hostname}" = darwin.lib.darwinSystem{
-          system = "aarch64-darwin";
-          specialArgs = { inherit inputs outputs packages secrets; };
-          modules = [
-            ./hosts/darwin
-          ];
-        };
+      hermes = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs outputs packages secrets; };
+        modules = [ ./hosts/hermes ];
       };
-
-      # Standalone home-manager configuration entrypoint
-      # Available through 'home-manager switch --flake .#your-username@your-hostname'
-      homeConfigurations = {
-        "${secrets.hosts.work-mac.username}@${secrets.hosts.work-mac.hostname}" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = { inherit inputs outputs secrets; };
-          modules = [
-            ./home-manager/darwin
-          ];
-        };
+      serenity = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs outputs packages secrets; };
+        modules = [ ./hosts/serenity ];
       };
     };
+
+    # nix-darwin configuration entrypoint
+    # Available through 'darwin-rebuild switch --flake .#your-hostname'
+    darwinConfigurations = {
+      "${secrets.hosts.work-mac.hostname}" = darwin.lib.darwinSystem{
+        system = "aarch64-darwin";
+        specialArgs = { inherit inputs outputs packages secrets; };
+        modules = [ ./hosts/darwin ];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager switch --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      "${secrets.hosts.work-mac.username}@${secrets.hosts.work-mac.hostname}" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs outputs secrets; };
+        modules = [ ./home-manager/darwin ];
+      };
+    };
+  };
 }
