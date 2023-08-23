9df0b3ecea338157b4ef7052dca3a89895f8992b
Winston R. Milling <Winston@Milli.ng>
2023-08-22 22:32:38 -0500

    Riker: Build on serenity

---
+++ hosts/riker/default.nix
@@ -0,0 +1,51 @@
+{ config, inputs, outputs, secrets, lib, pkgs, ... }:
+
+{  
+  imports =
+    [ 
+      inputs.hardware.nixosModules.pine64-pinebook-pro
+      inputs.home-manager.nixosModules.home-manager
+      ./hardware.nix
+      ../common/laptop.nix
+      ../common/addons/development.nix
+      ../common/addons/k8s-utils.nix
+      ../common/addons/tailscale.nix
+      ../common/addons/virtualization.nix
+      ../common/addons/zram.nix
+    ];
+  
+  networking = {
+    hostName = "riker";
+    domain = secrets.hosts.riker.domain;
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.kernelParams = lib.mkAfter [ "console=tty0" ];
+
+  boot.initrd.luks.devices = {
+    cryptroot = {
+      device = "/dev/disk/by-uuid/f9618ef7-0e79-4181-8a05-d95c300bb10f";
+      allowDiscards = true;
+      preLVM = true;
+    };
+  };
+
+  environment.etc = {
+    crypttab = {
+      text = ''
+        nvmecrypt /dev/disk/by-uuid/92cf1e12-72a6-48fd-911f-5249183e5c64 /home/luks/nvme.key luks
+      '';
+      mode = "0440";
+    };
+  };
+	
+	home-manager = {
+    extraSpecialArgs = { inherit inputs outputs; };
+    users = {
+      w4cbe = import ../../home-manager/riker;
+    };
+  };
+
+  system.stateVersion = "23.05"; 
+}
---
+++ hosts/riker/hardware.nix
@@ -0,0 +1,45 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [ 
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "usbhid" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  # boot.kernelPatches = [
+  #   {
+  #     # Undervolt and Overclock for Pinebook Pro RK3399
+  #     name = "pbp-undervolt-overclock";
+  #     patch = ./patches/0001-RK3399-Undervolt-and-Overclock-dtb-for-Pinebook-Pro.patch;
+  #   }
+  #   {
+  #     # All of https://github.com/megous/linux patches in one
+  #     name = "megous-all";
+  #     patch = ./patches/0002-megous-6.0.x-all.patch;
+  #   }
+  # ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/34035012-4604-43ed-8df5-0a0294857c5e";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = { 
+    device = "/dev/disk/by-uuid/12A6-CC3E";
+    fsType = "vfat";
+  };
+
+  fileSystems."/mnt/NVMe" = {
+    device = "/dev/disk/by-uuid/a74ab374-fc3a-4f76-9505-36803c533acb";
+    fsType = "ext4";
+  };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+}
