d12c66e1bbf32a3437a0d71714e5ceaee174d3ff
Winston R. Milling <Winston@Milli.ng>
2023-09-02 12:17:55 -0500

    Riker: Increase i3status intervals

--- home-manager/riker/i3status-rust.nix
+++ home-manager/riker/i3status-rust.nix
@@ -22,28 +22,29 @@ in
 
       [[block]]
       block = "cpu"
-      interval = 5
+      interval = 10
 
       [[block]]
       block = "memory"
       format = " $icon $mem_avail.eng(p:M,w:3) " 
+      interval = 30
 
       [[block]]
       block = "temperature"
-      interval = 10
+      interval = 30
       format = " $icon $average "
       chip = "cpu_thermal-*"
 
       [[block]]
       block = "temperature"
-      interval = 10
+      interval = 30
       format = " $icon $average "
       chip = "gpu_thermal-*"
 
       [[block]]
       block = "disk_space"
       path = "/"
-      interval = 20
+      interval = 60
       warning = 20.0
       alert = 10.0
       format = " $icon $available.eng(p:G,w:3) "
@@ -51,7 +52,7 @@ in
       [[block]]
       block = "disk_space"
       path = "/mnt/NVMe"
-      interval = 20
+      interval = 60
       warning = 20.0
       alert = 10.0
       format = " $icon $available.eng(p:G,w:3) "
@@ -59,12 +60,12 @@ in
       [[block]]
       block = "battery"
       device = "cw2015-battery"
-      interval = 10
+      interval = 30
 
       [[block]]
       block = "custom"
       command = "echo '\uf0e7 ' $(${bat-draw}/bin/bat-draw)"
-      interval = 10
+      interval = 20
 
       [[block]]
       block = "time"
