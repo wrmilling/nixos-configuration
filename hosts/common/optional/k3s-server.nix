895eca95b93e46dd13ad08e143ed3e6f5b0eddd0
Winston R. Milling <Winston@Milli.ng>
2023-10-14 09:18:00 -0500

    K3s: Add ports for pihole and emqx

--- hosts/common/optional/k3s-server.nix
+++ hosts/common/optional/k3s-server.nix
@@ -7,15 +7,21 @@ let
 in
 {
   # Port Requirements:
+  # 53 - PiHole (TCP+UDP)
   # 443 - Nginx Ingress (TCP)
+  # 1883 - EMQX Listener (TCP)
   # 5353 - Home Assistant Multicast (TCP+UDP)
   # 6443 - KubeApi (TCP)
   # 7472 - MetalLB (TCP+UDP)
   # 7946 - MetalLB (TCP+UDP)
+  # 8083 - EMQX WS (TCP)
+  # 8084 - EMQX WSS (TCP)
   # 8123 - Home Assistant (TCP+UDP)
+  # 8883 - EMQX SSL/TLS (TCP)
+  # 9617 - PiHole Metrics Exporter
   # 10250 - Metrics Server (TCP)
-  networking.firewall.allowedTCPPorts = [ 443 5353 6443 7472 7946 8123 10250 ];
-  networking.firewall.allowedUDPPorts = [ 5353 7472 7946 8123 ];
+  networking.firewall.allowedTCPPorts = [ 53 443 1883 5353 6443 7472 7946 8083 8084 8123 8883 9617 10250 ];
+  networking.firewall.allowedUDPPorts = [ 53 5353 7472 7946 8123 ];
   services.k3s = {
     enable = true;
     package = k3s-package;
