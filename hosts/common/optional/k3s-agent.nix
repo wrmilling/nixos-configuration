4d9a8a25372e5648052e72cf3605cfa3c38e1049
Winston R. Milling <Winston@Milli.ng>
2023-10-14 08:12:40 -0500

    K3s: Explicitly allow 443

--- hosts/common/optional/k3s-agent.nix
+++ hosts/common/optional/k3s-agent.nix
@@ -5,7 +5,7 @@ let
 in
 {
   # This is required so that pod can reach the API server (running on port 6443 by default) and Metrics (10250)
-  networking.firewall.allowedTCPPorts = [ 6443 10250 ];
+  networking.firewall.allowedTCPPorts = [ 443 6443 10250 ];
   services.k3s = {
     enable = true;
     package = k3s-package;
