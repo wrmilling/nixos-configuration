f99cb5220f5dbfa2e18fd7ca9727385a314c1296
Winston R. Milling <Winston@Milli.ng>
2023-10-14 12:21:03 -0500

    K3s: Make firewall common

---
+++ hosts/common/optional/k3s-firewall.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, lib, ... }:
+
+{
+  networking.firewall.allowedTCPPorts = [
+    53 # PiHole (TCP+UDP)
+    443 # Nginx Ingress (TCP)
+    1883 # EMQX Listener (TCP)
+    5353 # Home Assistant Multicast (TCP+UDP)
+    6443 # KubeApi (TCP)
+    7472 # MetalLB (TCP+UDP)
+    7946 # MetalLB (TCP+UDP)
+    8080 # CSI RBD Plugin Metrics (TCP)
+    8081 # CSI RBD Plugin Metrics (TCP)
+    8083 # EMQX WS (TCP)
+    8084 # EMQX WSS (TCP)
+    8123 # Home Assistant (TCP+UDP)
+    8883 # EMQX SSL/TLS (TCP)
+    9100 # Node Exporter Metrics (TCP)
+    9617 # PiHole Metrics Exporter (TCP)
+    10250 # Metrics Server (TCP)
+    10254 # MetalLB Metrics (TCP)
+    12321 # HASS VSCode (TCP)
+  ];
+  networking.firewall.allowedUDPPorts = [
+    53 # PiHole (TCP+UDP)
+    5353 # Home Assistant Multicast (TCP+UDP)
+    7472 # MetalLB (TCP+UDP)
+    7946 # MetalLB (TCP+UDP)
+    8123 # Home Assistant (TCP+UDP)
+  ];
+}
--- hosts/common/optional/k3s-server.nix
+++ hosts/common/optional/k3s-server.nix
@@ -6,25 +6,11 @@ let
   k3s-package = pkgs.unstable.k3s;
 in
 {
-  # Port Requirements:
-  # 53 - PiHole (TCP+UDP)
-  # 443 - Nginx Ingress (TCP)
-  # 1883 - EMQX Listener (TCP)
-  # 5353 - Home Assistant Multicast (TCP+UDP)
-  # 6443 - KubeApi (TCP)
-  # 7472 - MetalLB (TCP+UDP)
-  # 7946 - MetalLB (TCP+UDP)
-  # 8083 - EMQX WS (TCP)
-  # 8084 - EMQX WSS (TCP)
-  # 8123 - Home Assistant (TCP+UDP)
-  # 8883 - EMQX SSL/TLS (TCP)
-  # 9100 - Node Exporter Metrics (TCP)
-  # 9617 - PiHole Metrics Exporter
-  # 10250 - Metrics Server (TCP)
-  # 10254 - MetalLB Metrics (TCP)
-  # 12321 - HASS VSCode (TCP)
-  networking.firewall.allowedTCPPorts = [ 53 443 1883 5353 6443 7472 7946 8083 8084 8123 8883 9100 9617 10250 10254 12321 ];
-  networking.firewall.allowedUDPPorts = [ 53 5353 7472 7946 8123 ];
+  imports =
+    [
+      ./k3s-firewall.nix
+    ];
+
   services.k3s = {
     enable = true;
     package = k3s-package;
