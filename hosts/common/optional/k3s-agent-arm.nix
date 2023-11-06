af25cac3fd46128582016b478dfca1696347fa6d
Winston R. Milling <Winston@Milli.ng>
2023-11-05 21:00:27 -0600

    K3s: Forgot arm taint, copy/past fix for now

---
+++ hosts/common/optional/k3s-agent-arm.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, lib, ... }:
+let
+  secrets = import ../../../secrets/secrets.nix;
+  k3s-package = pkgs.unstable.k3s;
+in
+{
+  imports =
+    [
+      ./k3s-firewall.nix
+    ];
+
+  services.k3s = {
+    enable = true;
+    package = k3s-package;
+    role = "agent";
+    serverAddr = lib.mkDefault secrets.k3s.server.addr;
+    token = lib.mkDefault secrets.k3s.agent.nodeToken;
+    extraFlags = "--node-label \"k3s-upgrade=false\" --node-taint \"arm=true:NoExecute\""; # Optionally add additional args to k3s
+  };
+
+  environment.systemPackages = [ k3s-package ];
+}
