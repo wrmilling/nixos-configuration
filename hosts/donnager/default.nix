3c9dfdb4c869d5b45bf12fdf834370d9bce6be24
Winston R. Milling <Winston@Milli.ng>
2023-11-28 10:30:49 -0600

    Donnager: Add aarch64 remote builder

--- hosts/donnager/default.nix
+++ hosts/donnager/default.nix
@@ -10,6 +10,7 @@
       ../common/optional/development.nix
       ../common/optional/gaming.nix
       ../common/optional/k8s-utils.nix
+      ../common/optional/nixbuild-client.nix
       ../common/optional/printing.nix
       ../common/optional/tailscale.nix
       ../common/optional/virtualization.nix
