51d26e51b3738697c0fe4ba2e387fed42c685e7c
Winston R. Milling <Winston@Milli.ng>
2023-11-30 11:30:56 -0600

    K8s: Add cnpg plugin for kubectl

--- hosts/common/optional/k8s-utils.nix
+++ hosts/common/optional/k8s-utils.nix
@@ -12,6 +12,7 @@
     kubectl-example # output example kubernetes types
     kubectl-view-allocations # view kubernetes allocations
     kubectl-view-secret # view kubernetes secrets without piping and decoding
+    kubectl-cnpg # CNPG Plugin for kubectl
     kubecolor # colorize kubectl output
     unstable.kubernetes-helm # helm CLI
     kustomize # kustomize CLI for sadists
