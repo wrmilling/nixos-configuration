0b3bda1666dfb668649c331817041a62080ecd08
Winston R. Milling <Winston@Milli.ng>
2024-01-03 11:32:14 -0600

    Steam: Add remote play firewall ports

--- hosts/common/optional/gaming.nix
+++ hosts/common/optional/gaming.nix
@@ -3,6 +3,16 @@
 {
   programs.steam.enable = true;
 
+  networking.firewall.allowedTCPPorts = [
+    27036 # Steam Remote Play
+    27037 # Steam Remote Play
+  ];
+
+  networking.firewall.allowedUDPPorts = [
+    27031 # Steam Remote Play
+    27036 # Steam Remote Play
+  ];
+
   environment.systemPackages = with pkgs; [
     lutris
     heroic
