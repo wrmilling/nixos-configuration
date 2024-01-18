8d092d2d529d5df041010cc064f58e1c55fe6a0c
Winston R. Milling <Winston@Milli.ng>
2024-01-18 09:43:48 -0600

    Goku: Add install time changes

--- hosts/goku/hardware.nix
+++ hosts/goku/hardware.nix
@@ -9,23 +9,28 @@
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "virtio_pci" "virtio_scsi" "usbhid"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "virtio_pci" "virtio_scsi" "usbhid" "ata_piix" "uhci_hcd"];  boot.initrd.kernelModules = [];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = [];
+  boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/25f0a8ee-9767-447d-8f58-18f995ecd203";
-    fsType = "ext4";
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/ba398682-9547-45a5-858a-aad7620c10a3";
+      fsType = "ext4";
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/D521-E1D5";
-    fsType = "vfat";
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/1FC5-9E05";
+      fsType = "vfat";
+    };
 
-  swapDevices = [];
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 8*1024;
+  } ];
 
+  # Ensure consistent network interface name
+  boot.kernelParams = [ "net.ifnames=0" ];
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
