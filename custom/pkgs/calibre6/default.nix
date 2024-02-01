ff0f7ff38a5ef34485d0fc1f72a7d09970fff379
Winston R. Milling <Winston@Milli.ng>
2024-02-01 10:53:07 -0600

    Flake: Format Repo

--- custom/pkgs/calibre6/default.nix
+++ custom/pkgs/calibre6/default.nix
@@ -1,35 +1,35 @@
-{ lib
-, stdenv
-, fetchurl
-, cmake
-, fetchpatch
-, fontconfig
-, hunspell
-, hyphen
-, icu
-, imagemagick
-, libjpeg
-, libmtp
-, libpng
-, libstemmer
-, libuchardet
-, libusb1
-, pkg-config
-, podofo
-, poppler_utils
-, python3Packages
-, qmake
-, qtbase
-, qtwayland
-, removeReferencesTo
-, speechd
-, sqlite
-, wrapQtAppsHook
-, xdg-utils
-, wrapGAppsHook
-, unrarSupport ? false
+{
+  lib,
+  stdenv,
+  fetchurl,
+  cmake,
+  fetchpatch,
+  fontconfig,
+  hunspell,
+  hyphen,
+  icu,
+  imagemagick,
+  libjpeg,
+  libmtp,
+  libpng,
+  libstemmer,
+  libuchardet,
+  libusb1,
+  pkg-config,
+  podofo,
+  poppler_utils,
+  python3Packages,
+  qmake,
+  qtbase,
+  qtwayland,
+  removeReferencesTo,
+  speechd,
+  sqlite,
+  wrapQtAppsHook,
+  xdg-utils,
+  wrapGAppsHook,
+  unrarSupport ? false,
 }:
-
 stdenv.mkDerivation (finalAttrs: {
   pname = "calibre";
   version = "6.29.0";
@@ -39,20 +39,21 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-w9mvMKm76w5sDfW0OYxhZuhIOYKdUH3tpiGlpKNC2kM=";
   };
 
-  patches = [
-    #  allow for plugin update check, but no calibre version check
-    (fetchpatch {
-      name = "0001-only-plugin-update.patch";
-      url = "https://raw.githubusercontent.com/debian-calibre/calibre/debian/${finalAttrs.version}+ds-1/debian/patches/0001-only-plugin-update.patch";
-      hash = "sha256-uL1mSjgCl5ZRLbSuKxJM6XTfvVwog70F7vgKtQzQNEQ=";
-    })
-    (fetchpatch {
-      name = "0007-Hardening-Qt-code.patch";
-      url = "https://raw.githubusercontent.com/debian-calibre/calibre/debian/${finalAttrs.version}+ds-1/debian/patches/hardening/0007-Hardening-Qt-code.patch";
-      hash = "sha256-eTzwo8aAIJnZTIZ/8DqCQi3ZbKxycEdiv+UxRuxo12g=";
-    })
-  ]
-  ++ lib.optional (!unrarSupport) ./dont_build_unrar_plugin.patch;
+  patches =
+    [
+      #  allow for plugin update check, but no calibre version check
+      (fetchpatch {
+        name = "0001-only-plugin-update.patch";
+        url = "https://raw.githubusercontent.com/debian-calibre/calibre/debian/${finalAttrs.version}+ds-1/debian/patches/0001-only-plugin-update.patch";
+        hash = "sha256-uL1mSjgCl5ZRLbSuKxJM6XTfvVwog70F7vgKtQzQNEQ=";
+      })
+      (fetchpatch {
+        name = "0007-Hardening-Qt-code.patch";
+        url = "https://raw.githubusercontent.com/debian-calibre/calibre/debian/${finalAttrs.version}+ds-1/debian/patches/hardening/0007-Hardening-Qt-code.patch";
+        hash = "sha256-eTzwo8aAIJnZTIZ/8DqCQi3ZbKxycEdiv+UxRuxo12g=";
+      })
+    ]
+    ++ lib.optional (!unrarSupport) ./dont_build_unrar_plugin.patch;
 
   prePatch = ''
     sed -i "s@\[tool.sip.project\]@[tool.sip.project]\nsip-include-dirs = [\"${python3Packages.pyqt6}/${python3Packages.python.sitePackages}/PyQt6/bindings\"]@g" \
@@ -74,64 +75,69 @@ stdenv.mkDerivation (finalAttrs: {
     wrapQtAppsHook
   ];
 
-  buildInputs = [
-    fontconfig
-    hunspell
-    hyphen
-    icu
-    imagemagick
-    libjpeg
-    libmtp
-    libpng
-    libstemmer
-    libuchardet
-    libusb1
-    podofo
-    poppler_utils
-    qtbase
-    qtwayland
-    sqlite
-    xdg-utils
-  ] ++ (
-    with python3Packages; [
-      (apsw.overrideAttrs (oldAttrs: {
-        setupPyBuildFlags = [ "--enable=load_extension" ];
-      }))
-      beautifulsoup4
-      css-parser
-      cssselect
-      python-dateutil
-      dnspython
-      faust-cchardet
-      feedparser
-      html2text
-      html5-parser
-      lxml
-      markdown
-      mechanize
-      msgpack
-      netifaces
-      pillow
-      pychm
-      pyqt-builder
-      pyqt6
-      python
-      regex
-      sip
-      setuptools
-      speechd
-      zeroconf
-      jeepney
-      pycryptodome
-      # the following are distributed with calibre, but we use upstream instead
-      odfpy
-    ] ++ lib.optionals (lib.lists.any (p: p == stdenv.hostPlatform.system) pyqt6-webengine.meta.platforms) [
-      # much of calibre's functionality is usable without a web
-      # browser, so we enable building on platforms which qtwebengine
-      # does not support by simply omitting qtwebengine.
-      pyqt6-webengine
-    ] ++ lib.optional (unrarSupport) unrardll
-  );
+  buildInputs =
+    [
+      fontconfig
+      hunspell
+      hyphen
+      icu
+      imagemagick
+      libjpeg
+      libmtp
+      libpng
+      libstemmer
+      libuchardet
+      libusb1
+      podofo
+      poppler_utils
+      qtbase
+      qtwayland
+      sqlite
+      xdg-utils
+    ]
+    ++ (
+      with python3Packages;
+        [
+          (apsw.overrideAttrs (oldAttrs: {
+            setupPyBuildFlags = ["--enable=load_extension"];
+          }))
+          beautifulsoup4
+          css-parser
+          cssselect
+          python-dateutil
+          dnspython
+          faust-cchardet
+          feedparser
+          html2text
+          html5-parser
+          lxml
+          markdown
+          mechanize
+          msgpack
+          netifaces
+          pillow
+          pychm
+          pyqt-builder
+          pyqt6
+          python
+          regex
+          sip
+          setuptools
+          speechd
+          zeroconf
+          jeepney
+          pycryptodome
+          # the following are distributed with calibre, but we use upstream instead
+          odfpy
+        ]
+        ++ lib.optionals (lib.lists.any (p: p == stdenv.hostPlatform.system) pyqt6-webengine.meta.platforms) [
+          # much of calibre's functionality is usable without a web
+          # browser, so we enable building on platforms which qtwebengine
+          # does not support by simply omitting qtwebengine.
+          pyqt6-webengine
+        ]
+        ++ lib.optional unrarSupport unrardll
+    );
 
   installPhase = ''
     runHook preInstall
@@ -188,7 +194,7 @@ stdenv.mkDerivation (finalAttrs: {
     done
   '';
 
-  disallowedReferences = [ podofo.dev ];
+  disallowedReferences = [podofo.dev];
 
   meta = {
     homepage = "https://calibre-ebook.com";
@@ -200,10 +206,11 @@ stdenv.mkDerivation (finalAttrs: {
       free and open source and great for both casual users and computer experts.
     '';
     changelog = "https://github.com/kovidgoyal/calibre/releases/tag/v${finalAttrs.version}";
-    license = if unrarSupport
-              then lib.licenses.unfreeRedistributable
-              else lib.licenses.gpl3Plus;
-    maintainers = with lib.maintainers; [ pSub ];
+    license =
+      if unrarSupport
+      then lib.licenses.unfreeRedistributable
+      else lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [pSub];
     platforms = lib.platforms.unix;
     broken = stdenv.isDarwin;
   };
