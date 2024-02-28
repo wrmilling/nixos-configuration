5fe33992ba888fea13ddfad05068dd521f2f3289
Winston R. Milling <Winston@Milli.ng>
2024-02-28 13:10:25 -0600

    Flake: Home-Manager to unstable

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     # nixpkgs-lilyinstarlight.url = "github:lilyinstarlight/nixpkgs/tmp/cosmic";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Hardware
