0459c8759cd86934c89b5c5dd5af3350285e45b3
Winston R. Milling <Winston@Milli.ng>
2024-02-28 12:28:37 -0600

    Bill: Setup as actions runner

--- hosts/bill/default.nix
+++ hosts/bill/default.nix
@@ -11,7 +11,7 @@
     inputs.home-manager.nixosModules.home-manager
     ./hardware.nix
     ../common/server.nix
-    ../common/optional/webhost.nix
+    # ../common/optional/webhost.nix
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
@@ -23,6 +23,26 @@
     hostName = "bill"; # Define your hostname.
     domain = secrets.hosts.bill.domain;
     nameservers = secrets.hosts.bill.nameservers;
+  };
+
+  virtualisation.docker.enable = true;
+  services.gitea-actions-runner = {
+    package = pkgs.unstable.forgejo-actions-runner;
+    instances.goku = {
+      enable = true;
+      name = "goku";
+      labels = [
+        "alpine:docker://alpine:3.19.0"
+        "alpine-latest:docker://alpine:latest"
+        "debian-bullseye:docker://debian:bullseye"
+        "debian-bookworm:docker://debian:bookworm"
+        "debian-latest:docker://debian:latest"
+        "ubuntu-latest:docker://node:18-bullseye"
+        "docker:docker://alpine:3.19.0"
+      ];
+      url = secrets.forgejo.domain;
+      token = secrets.forgejo.runnerToken;
+    };
   };
 
   home-manager = {
