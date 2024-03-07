f8b9ef7525bc09e22e5c1c1476635b006524c614
Winston R. Milling <Winston@Milli.ng>
2024-03-07 09:07:50 -0600

    Docker: add ;

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -7,6 +7,6 @@
   virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true;
-    package = (docker.override(args: { buildxSupport = true; }))
+    package = (docker.override(args: { buildxSupport = true; }));
   };
 }
