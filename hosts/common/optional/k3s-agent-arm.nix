35001265dfcbddddd3aaacb6890fb3454f081927
Winston R. Milling <Winston@Milli.ng>
2024-10-02 12:46:37 -0500

    k3s: prune container images after 1w

--- hosts/common/optional/k3s-agent-arm.nix
+++ hosts/common/optional/k3s-agent-arm.nix
@@ -18,12 +18,12 @@ in
     serverAddr = lib.mkDefault secrets.k3s.server.addr;
     token = lib.mkDefault secrets.k3s.agent.nodeToken;
     extraFlags = "--node-label \"k3s-upgrade=false\" --node-taint \"arm=true:NoExecute\""; # Optionally add additional args to k3s
-    # extraKubeletConfig = {
-    #   featureGates = {
-    #     ImageMaximumGCAge = "true";
-    #   };
-    #   imageMaximumGCAge = "1w";
-    # };
+    extraKubeletConfig = {
+      featureGates = {
+        ImageMaximumGCAge = true;
+      };
+      imageMaximumGCAge = "168h";
+    };
   };
 
   environment.systemPackages = [ k3s-package ];
--- hosts/common/optional/k3s-server.nix
+++ hosts/common/optional/k3s-server.nix
@@ -19,12 +19,12 @@ in
     role = "server";
     token = lib.mkDefault secrets.k3s.agent.nodeToken;
     extraFlags = "--tls-san ${hostname}.${domain} --disable servicelb --disable traefik --disable local-storage --flannel-backend=host-gw --node-taint \"node-role.kubernetes.io/master=true:NoSchedule\" --node-label \"k3s-upgrade=false\"";
-    # extraKubeletConfig = {
-    #   featureGates = {
-    #     ImageMaximumGCAge = "true";
-    #   };
-    #   imageMaximumGCAge = "1w";
-    # };
+    extraKubeletConfig = {
+      featureGates = {
+        ImageMaximumGCAge = true;
+      };
+      imageMaximumGCAge = "168h";
+    };
   };
 
   programs.nbd.enable = true;
