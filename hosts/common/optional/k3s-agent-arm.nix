5cb06514b963ae2836e143f354b8db317daa8daf
Winston R. Milling <Winston@Milli.ng>
2024-10-02 12:05:36 -0500

    k3s: prune container images after 1w

--- hosts/common/optional/k3s-agent-arm.nix
+++ hosts/common/optional/k3s-agent-arm.nix
@@ -18,6 +18,12 @@ in
     serverAddr = lib.mkDefault secrets.k3s.server.addr;
     token = lib.mkDefault secrets.k3s.agent.nodeToken;
     extraFlags = "--node-label \"k3s-upgrade=false\" --node-taint \"arm=true:NoExecute\""; # Optionally add additional args to k3s
+    extraKubeletConfig = {
+      featureGates= {
+        ImageMaximumGCAge = "true";
+      };
+      imageMaximumGCAge = "1w";
+    };
   };
 
   environment.systemPackages = [ k3s-package ];
