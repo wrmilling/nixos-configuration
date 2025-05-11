00551f7a84f391317d181413ad60eb0afa3439fd
wrmilling <winston@milli.ng>
2025-05-11 15:06:44 -0500

    cousteau: Add default config

---
+++ hosts/cousteau/default.nix
@@ -0,0 +1,28 @@
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, lib, pkgs, inputs, outputs, ... }:
+
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    ../common/default.nix
+  ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "w4cbe";
+  wsl.wslConf.network.hostname = "cousteau";
+
+  home-manager = {
+    extraSpecialArgs = {
+      inherit inputs outputs;
+    };
+    users = {
+      w4cbe = import ../../home-manager/server;
+    };
+  };
+
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  system.stateVersion = "24.11";
+}
