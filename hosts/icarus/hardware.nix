370a25d5bf0611bbaed48c7a782d8dc9f93362c2
Winston R. Milling <Winston@Milli.ng>
2025-08-16 14:17:30 -0500

    Icarus: Update for new NVMe

--- hosts/icarus/hardware.nix
+++ hosts/icarus/hardware.nix
@@ -24,15 +24,15 @@
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/d1691757-13d4-437f-98d0-6ba92a7e892f";
+    device = "/dev/disk/by-uuid/adfc6416-72ba-4c16-813b-c51dd746c51f";
     fsType = "ext4";
   };
 
   boot.initrd.luks.devices."cryptroot".device =
-    "/dev/disk/by-uuid/7d2c1c69-e930-4819-bc32-2f1d482b1412";
+    "/dev/disk/by-uuid/49badd4e-2c54-4ec0-a4f4-893e664df466";
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/DA96-A70D";
+    device = "/dev/disk/by-uuid/B107-307C";
     fsType = "vfat";
     options = [ "fmask=0022" "dmask=0022" ];
   };
