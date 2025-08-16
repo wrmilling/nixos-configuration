d26f168039a09a41a689e6d4af2140e24d29bfdf
wrmilling <winston@milli.ng>
2025-08-15 23:56:32 -0500

    Icarus: Add specific hardware definitions

--- hosts/icarus/hardware.nix
+++ hosts/icarus/hardware.nix
@@ -9,23 +9,31 @@
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot.initrd.availableKernelModules = [
-    # TBD
+    "nvme"
+    "xhci_pci"
+    "usb_storage"
+    "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  # Fingerprint Reader
+  services.fprintd.enable = true;
+  services.fprintd.tod.enable = true;
+
   fileSystems."/" = {
-    device = "TBD";
+    device = "/dev/disk/by-uuid/d1691757-13d4-437f-98d0-6ba92a7e892f";
     fsType = "ext4";
   };
 
   boot.initrd.luks.devices."cryptroot".device =
-    "TBD";
+    "/dev/disk/by-uuid/7d2c1c69-e930-4819-bc32-2f1d482b1412";
 
   fileSystems."/boot" = {
-    device = "TBD";
+    device = "/dev/disk/by-uuid/DA96-A70D";
     fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
   };
 
   swapDevices = [ ];
