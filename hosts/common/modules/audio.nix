b185ac56eaf02ddbf2f6b15ea669b5ae63915396
Winston R. Milling <Winston@Milli.ng>
2025-08-24 16:15:19 -0500

    Audio: Add extra bluetooth codecs

--- hosts/common/modules/audio.nix
+++ hosts/common/modules/audio.nix
@@ -23,6 +23,24 @@
           "hsp_ag"
           "hfp_hf"
           "hfp_ag"
+          "a2dp_sink"
+          "a2dp_source"
+          "bap_sink"
+          "bap_source"
+        ];
+        "bluez5.codecs" = [
+          "ldac"
+          "aptx"
+          "aptx_ll_duplex"
+          "aptx_ll"
+          "aptx_hd"
+          "opus_05_pro"
+          "opus_05_71"
+          "opus_05_51"
+          "opus_05"
+          "opus_05_duplex"
+          "aac"
+          "sbc_xq"
         ];
       };
     };
