d114cc97d347630a03b38b01cf0102da32805e2f
Winston R. Milling <Winston@Milli.ng>
2025-10-19 20:55:59 -0500

    Goku: add to sops, deploy forgejo actions runner

--- hosts/goku/default.nix
+++ hosts/goku/default.nix
@@ -4,7 +4,7 @@
   outputs,
   pkgs,
   lib,
-  #secrets,
+  secrets,
   ...
 }:
 {
@@ -21,79 +21,45 @@
 
   networking = {
     hostName = "goku"; # Define your hostname.
-    # domain = secrets.hosts.common.domain;
-    # nameservers = secrets.hosts.common.nameservers;
-    # interfaces.ens3 = {
-    #   ipv4.addresses = [{
-    #     address = secrets.hosts.goku.ipAddress;
-    #     prefixLength = secrets.hosts.goku.prefixLength;
-    #   }];
-    # };
-    # defaultGateway = {
-    #   address = secrets.hosts.goku.gateway;
-    #   interface = "ens3";
-    # };
+    domain = secrets.hosts.common.domain;
+    nameservers = secrets.hosts.common.nameservers;
+    interfaces.ens3 = {
+      ipv4.addresses = [{
+        address = secrets.hosts.goku.ipAddress;
+        prefixLength = secrets.hosts.goku.prefixLength;
+      }];
+    };
+    defaultGateway = {
+      address = secrets.hosts.goku.gateway;
+      interface = "ens3";
+    };
   };
 
-  # sops.secrets."forgejo/runnerToken" = {
-  #   sopsFile = ../../secrets/forgejo.yaml;
-  # };
+  sops.secrets."forgejo/runnerToken" = {
+    sopsFile = ../../secrets/forgejo.yaml;
+  };
 
-  # sops.secrets."renovate/token" = {
-  #   sopsFile = ../../secrets/renovate.yaml;
-  # };
-
-  # sops.secrets."renovate/github_token" = {
-  #   sopsFile = ../../secrets/renovate.yaml;
-  # };
-
-  # sops.secrets."renovate/git_private_key" = {
-  #   sopsFile = ../../secrets/renovate.yaml;
-  # };
-
-  # virtualisation.docker.enable = true;
-  # services.gitea-actions-runner = {
-  #   package = pkgs.forgejo-actions-runner;
-  #   instances.goku = {
-  #     enable = true;
-  #     name = "goku";
-  #     settings = {
-  #       runner.fetch_interval = "15s";
-  #       container.docker_host = "automount";
-  #     };
-  #     labels = [
-  #       "alpine:docker://alpine:3.22.2"
-  #       "alpine-latest:docker://alpine:latest"
-  #       "alpine-tokyo:docker://${secrets.forgejo.domain}/wrmilling/alpine-tokyo:3.22.2-1"
-  #       "alpine-tokyo-latest:docker://${secrets.forgejo.domain}/wrmilling/alpine-tokyo:latest"
-  #       "ubuntu-latest:docker://node:18-bullseye"
-  #     ];
-  #     url = "https://${secrets.forgejo.domain}";
-  #     tokenFile = config.sops.secrets."forgejo/runnerToken".path;
-  #   };
-  # };
-
-  # services.renovate = {
-  #   enable = true;
-  #   package = pkgs.renovate;
-  #   credentials = {
-  #     # Manually placed on machine
-  #     RENOVATE_TOKEN = config.sops.secrets."renovate/token".path;
-  #     RENOVATE_GITHUB_COM_TOKEN = config.sops.secrets."renovate/github_token".path;
-  #     RENOVATE_GIT_PRIVATE_KEY = config.sops.secrets."renovate/git_private_key".path;
-  #   };
-  #   runtimePackages = [
-  #     pkgs.gnupg
-  #   ];
-  #   settings = {
-  #     endpoint = "https://${secrets.forgejo.domain}";
-  #     gitAuthor = "Renovate Bot <${secrets.forgejo.renovateEmail}>";
-  #     platform = "gitea";
-  #     autodiscover = true;
-  #   };
-  #   # Every 10 minutes
-  #   schedule = "*:0/30";
-  # };
+  virtualisation.docker.enable = true;
+  services.gitea-actions-runner = {
+    package = pkgs.forgejo-actions-runner;
+    instances.goku = {
+      enable = true;
+      name = "goku";
+      settings = {
+        runner.fetch_interval = "15s";
+        container.docker_host = "automount";
+      };
+      labels = [
+        "alpine:docker://alpine:3.22.2"
+        "alpine-latest:docker://alpine:latest"
+        "alpine-tokyo:docker://${secrets.forgejo.domain}/wrmilling/alpine-tokyo:3.22.2-1"
+        "alpine-tokyo-latest:docker://${secrets.forgejo.domain}/wrmilling/alpine-tokyo:latest"
+        "ubuntu-latest:docker://node:18-bullseye"
+      ];
+      url = "https://${secrets.forgejo.domain}";
+      tokenFile = config.sops.secrets."forgejo/runnerToken".path;
+    };
+  };
 
   home-manager = {
     extraSpecialArgs = {
