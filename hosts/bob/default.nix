f9a27bf7694f0e8e56cadb4569320c60f76770eb
Winston R. Milling <Winston@Milli.ng>
2025-10-21 12:53:34 -0500

    Bob: Add deployment user

--- hosts/bob/default.nix
+++ hosts/bob/default.nix
@@ -28,6 +28,18 @@
     nameservers = secrets.hosts.common.nameservers;
   };
 
+  users.users.deploy = {
+    uid = 1050;
+    shell = pkgs.sh;
+    isNormalUser = true;
+    home = "/home/deploy";
+    description = "Site Deployment User";
+    openssh.authorizedKeys.keys = [
+      ''command="${pkgs.rrsync}/bin/rrsync /var/www/${secrets.hosts.common.b_domain}/html",restrict ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCs2GqjWSQx1CnPj2QOclRD9jBHhc1B/juzdZITF7+EW9+GxV9uV4NZKJVqbbnSNuRIMlaVxuTOEi7ObsIFl7GZEimcenhUnbwlDva4OUtYwwVRpenhjTZTgWaVcU51JFV0rX9Q97pI4KY4Xte4oTIifRvuQp5hroqZKuuhwlnR552iSmKhYADJn0A8ofcsK58ue6SlPE6sEn9xRhtPxtRxMTqtxwqio/yzRDro9V/hxWVjEBCEthA+fVAlktLf+Ly/3+VaXERc9MxzPXhW8l0tEZAi+E75AjQOnz1+Z8CSc/pM5DDex7XUCrbLY1uNCPaoN+7OAh1jjsE67bXQPkzfJQkOBL2eKZQM4IJnmote43Adlzh6QY1okp+1q1a00NrgG0aSJgss4KpiXQelMxN9t+DwqUbGFXuG9IEk/3KREAm2AL2i5XilBwDiiZqX2a4/1rs0GzYIrLuUajnSLnS6eTpVwBHhGPT7LzctlArlVTu1WkoOjC/UQf1uhri0ZPcsoabvPcU6aJEELdJuwjQdpLVgVrB5PImVib83vJfHp0IdMCmbDh2YNrS64Nco5ejDjP3h5aMr3uBwJQsGfWx/ThyxLavBSRysR/SKI1nE/i3GY+GwysXEU85DL4e39l2QnhlexQDy6txFpco9x8kZwh92eaU1TNb14gU+TR5joQ==''
+      ''command="${pkgs.rrsync}/bin/rrsync /var/www/${secrets.hosts.common.b_domain}/html",restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBCkUs+kI8QS71lWZdxSi0Dny+llNM9uGLmWvNlpi7u8''
+    ];
+  };
+
   services.uptime-kuma = {
     enable = true;
     package = pkgs.uptime-kuma;
@@ -47,7 +59,7 @@
       # "${secrets.hosts.common.b_domain}" = {
       #   forceSSL = true;
       #   enableACME = true;
-      #   root = "/var/www/${secrets.hosts.common.b_domain}";
+      #   root = "/var/www/${secrets.hosts.common.b_domain}/html";
       # };
 
       "${secrets.hosts.common.m_domain}" = {
