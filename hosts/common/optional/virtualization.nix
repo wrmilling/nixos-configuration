a96d3a3f59cf9b189dd8c358e6d63f01ce00cecd
Winston R. Milling <Winston@Milli.ng>
2025-10-22 13:16:32 -0500

    Virtualization: Add libvirtd and virt-manager

--- hosts/common/optional/virtualization.nix
+++ hosts/common/optional/virtualization.nix
@@ -9,4 +9,11 @@
     pkgs.qemu
     pkgs.qemu-utils
   ];
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
+  };
+
+  programs.virt-manager.enable = true;
 }
