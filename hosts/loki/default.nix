5e688dd172948ba569562371f5aa1ca51dbb51fb
Winston R. Milling <Winston@Milli.ng>
2025-11-06 12:30:16 -0600

    Logi: add a few more usecases now that it is setup

--- hosts/loki/default.nix
+++ hosts/loki/default.nix
@@ -12,7 +12,14 @@
     inputs.home-manager.nixosModules.home-manager
     ./hardware.nix
     ../common/desktop.nix
+    ../common/optional/amateur-radio.nix
+    ../common/optional/development.nix
+    ../common/optional/docker-rootless.nix
+    ../common/optional/flipper.nix
+    ../common/optional/k8s-utils.nix
+    ../common/optional/printing.nix
     ../common/optional/secureboot.nix
+    ../common/optional/wireshark.nix
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
