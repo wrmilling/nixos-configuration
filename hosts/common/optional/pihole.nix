cc63a5bb7893c1092a9c0e8e9c0ecea1b5604a0b
Winston R. Milling <Winston@Milli.ng>
2025-11-13 22:44:41 -0600

    pihole: add sops, attempt custom domains

--- hosts/common/optional/pihole.nix
+++ hosts/common/optional/pihole.nix
@@ -10,11 +10,32 @@
     enable = true;
     openFirewallDNS = true;
     openFirewallWebserver = true;
+    useDnsmasqConfig = true;
+    lists = [
+      {
+        url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
+        description = "Steven Black's Unified Adlist";
+      }
+      {
+        url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt";
+        description = "HaGeZi's Pro DNS Blocklist";
+      }
+      {
+        url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-only/hosts";
+        description = "Steven Black's Why Wont You Think of the Children List";
+      }
+    ];
+  };
+
+  services.dnsmasq.settings = {
+    conf-file = config.sops.secrets."pihole/customHosts".path;
   };
 
   services.pihole-web = {
     enable = true;
     ports = [ "5353s" ];
   };
+
+  # config.sops.secrets."k3s/agent/nodeTokenFull".path;
 
 }
