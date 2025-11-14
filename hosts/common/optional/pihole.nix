f88ee0dcea4a22d5d64d1bf23df55b8b802521ec
Winston R. Milling <Winston@Milli.ng>
2025-11-13 23:59:18 -0600

    stubby: remove pinset for right now

--- hosts/common/optional/pihole.nix
+++ hosts/common/optional/pihole.nix
@@ -67,18 +67,18 @@
         {
           address_data = "1.1.1.1";
           tls_auth_name = "cloudflare-dns.com";
-          tls_pubkey_pinset = [{
-            digest = "sha256";
-            value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
-          }];
+          # tls_pubkey_pinset = [{
+          #   digest = "sha256";
+          #   value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
+          # }];
         }
         {
           address_data = "1.0.0.1";
           tls_auth_name = "cloudflare-dns.com";
-          tls_pubkey_pinset = [{
-            digest = "sha256";
-            value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
-          }];
+          # tls_pubkey_pinset = [{
+          #   digest = "sha256";
+          #   value = "GP8Knf7qBae+aIfythytMbYnL+yowaWVeD6MoLHkVRg=";
+          # }];
         }
       ];
     };
