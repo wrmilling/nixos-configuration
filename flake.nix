f813764068c39dd861580f36ff00880323429642
Winston R. Milling <Winston@Milli.ng>
2025-11-30 23:54:45 -0600

    Flake: update stable reference

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
     # Hardware
     hardware.url = "github:NixOS/nixos-hardware";
