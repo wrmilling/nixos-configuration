0be1f71efa86df25faaf12f773965174ff1a1795
Winston R. Milling <Winston@Milli.ng>
2026-01-31 11:48:51 -0600

    OpenCode: I didn't RTFM, its already imported

--- modules/home/components/terminal.development.nix
+++ modules/home/components/terminal.development.nix
@@ -96,7 +96,6 @@ in
         instructions = [
           "~/.copilot/copilot-instructions.md"
           ".github/copilot-instructions.md"
-          "AGENTS.md"
         ];
         plugin = [ "oh-my-opencode-slim" ];
         # Default to a cheap model for now
