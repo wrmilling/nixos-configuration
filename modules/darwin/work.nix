a800ce7f3ec011d189cca615f257afdb6f928f07
Winston R. Milling <Winston@Milli.ng>
2026-03-23 14:18:35 -0500

    Darwin: Add certs file for more situations

--- modules/darwin/work.nix
+++ modules/darwin/work.nix
@@ -18,4 +18,11 @@ in
       darwin.base.enable = true;
     };
   };
+
+  programs.fish.shellInit = ''
+      set -gx SSL_CERT_FILE /usr/local/munki/thd_certs.pem
+      set -gx NIX_SSL_CERT_FILE /usr/local/munki/thd_certs.pem
+      set -gx NODE_EXTRA_CA_CERTS /usr/local/munki/thd_certs.pem
+      set -gx REQUESTS_CA_BUNDLE /usr/local/munki/thd_certs.pem
+    '';
 }
