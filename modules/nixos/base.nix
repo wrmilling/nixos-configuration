71fd2b7724def027e7d1b359e160399ef11c3a81
Winston R. Milling <Winston@Milli.ng>
2026-07-02 14:31:28 -0500

    Gaming: Add star citizen cachix

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -38,11 +38,9 @@ in
         experimental-features = [ "nix-command" "flakes" ];
         substituters = [
           secrets.nixcache.hostname
-          "https://cache.nixos.org"
         ];
         trusted-public-keys = [
           secrets.nixcache.public_key
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         ];
       };
       settings.trusted-users = [
--- modules/nixos/components/starCitizen.nix
+++ modules/nixos/components/starCitizen.nix
@@ -13,5 +13,10 @@ in
 
   config = lib.mkIf cfg.enable {
     programs.rsi-launcher.enable = true;
+
+    nix.settings = {
+      substituters = [ "https://nix-citizen.cachix.org" ];
+      trusted-public-keys = [ "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo=" ];
+    };
   };
 }
