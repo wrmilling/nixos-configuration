d0df007c76c847e97f81172e2a892b2e03a432bd
Winston R. Milling <Winston@Milli.ng>
2026-07-22 14:50:22 -0500

    Obsidian: Configure some default vaults

--- modules/home/components/graphical.obsidian.nix
+++ modules/home/components/graphical.obsidian.nix
@@ -17,12 +17,23 @@ in
       enable = true;
       package = pkgs.obsidian;
 
+      defaultSettings.app = {
+        readableLineLength = true;
+        showLineNumber = true;
+        tabSize = 2;
+      };
+
       defaultSettings.communityPlugins = [
         { pkg = pkgs.obsidianPlugins.base-board; }
         { pkg = pkgs.obsidianPlugins.pandoc; }
         { pkg = pkgs.obsidianPlugins.dataview; }
         { pkg = pkgs.obsidianPlugins.advanced-tables; }
       ];
+
+      vaults = {
+        personal.target = ".obsidian/personal";
+        work.target = ".obsidian/work";
+      };
     };
   };
 }
