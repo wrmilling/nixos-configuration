4a767b82bdcec089137310c6dfcaf5aebbcb0478
Winston R. Milling <Winston@Milli.ng>
2026-09-01 13:26:41 -0500

    Rancher: doCheck = false; Upstream GO tries to bind TCP in sandbox

--- custom/overlays/default.nix
+++ custom/overlays/default.nix
@@ -36,6 +36,11 @@
       doCheck = !prev.stdenv.hostPlatform.isi686;
     };
 
+    # Go test suite tries to bind a TCP listener, wish the build sandbox blocks
+    rancher = prev.rancher.overrideAttrs {
+      doCheck = false;
+    }
+
     # nixpkgs PR #510918: linuxPackages.facetimehd 0.6.13 -> 0.7.0.1
     linuxPackagesFor =
       kernel:
